d835835c9b06e412e328a2c3a0247ecceb3f5fb8
allan <allan.flexmobile@gmail.com>
2023-02-10 15:45:07 -0300

    Commiti inicial

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:listinha/src/app_widget.dart';
+import 'package:listinha/src/home/home_page.dart';
+
+void main() {
+  runApp(
+    const AppWidget(),
+  );
+}
---
+++ lib/src/app_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:listinha/main.dart';
+import 'package:listinha/src/home/home_page.dart';
+
+import 'shared/themes/themes.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      home: const HomePage(),
+    );
+  }
+}
